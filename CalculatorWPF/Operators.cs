15f785d1655daefbebfbbc31cf76449be69faf4e
Gambar._ <gambarli.gambar@gmail.com>
2022-07-03 04:26:02 +0400

    Proje dosyası ekle.

---
+++ CalculatorWPF/Operators.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace CalculatorWPF
+{
+    internal class Operators
+    {
+        public string FirstValue { get; set; }
+        public string SecondValue { get; set; }
+        public OperationType OperationType { get; set; }
+        public string Result { get; set; }
+
+        public Operators()
+        {
+            FirstValue = String.Empty;
+            SecondValue = String.Empty;
+        }
+
+        public void Clear()
+        {
+            FirstValue = String.Empty;
+            SecondValue = String.Empty;
+            Result = String.Empty;
+        }
+        public string Calculate()
+        {
+            if (String.IsNullOrWhiteSpace(FirstValue))
+                return String.Empty;
+
+            if (String.IsNullOrWhiteSpace(SecondValue))
+                return FirstValue;
+
+
+            var firstValue = Double.Parse(FirstValue);
+            var secondValue = Double.Parse(SecondValue);
+            double result;
+            switch (OperationType)
+            {
+                case OperationType.Add:
+                    result = firstValue + secondValue;
+                    break;
+                case OperationType.Sub:
+                    result = firstValue - secondValue;
+                    break;
+                case OperationType.Mul:
+                    result = firstValue * secondValue;
+                    break;
+                case OperationType.Div:
+                    {
+                        if (secondValue == 0)
+                            throw new DivideByZeroException("Divide by zero");
+
+                        result = firstValue / secondValue;
+                    }
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            return result.ToString(CultureInfo.InvariantCulture);
+        }
+
+        public OperationType GetOperationType(string operation)
+        {
+            switch (operation)
+            {
+                case "+":
+                    return OperationType.Add;
+                case "-":
+                    return OperationType.Sub;
+                case "*":
+                    return OperationType.Mul;
+                case "/":
+                    return OperationType.Div;
+                case "=":
+                    return OperationType.Equal;
+                default:
+                    throw new InvalidOperationException("Unknown operation keyword");
+            }
+        }
+    }
+
+    public enum OperationType
+    {
+        Add,
+        Sub,
+        Mul,
+        Div,
+        Equal
+    }
+
+}
+
